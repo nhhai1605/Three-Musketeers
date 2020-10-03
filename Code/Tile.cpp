5665c1804c83716d62d8247c73c17fac4d6b4cd6
Hai Nguyen S3822918 <s3822918@rmit.edu.vn>
2020-10-03 20:30:40 +1000

    Add Tile.cpp and Tile.h

---
+++ Code/Tile.cpp
@@ -0,0 +1,50 @@
+#include "Tile.h"
+
+Tile::Tile(TileType tileType)
+{
+	this->tileType = tileType;
+}
+
+TileType Tile::getTileType()
+{
+	return tileType;
+}
+
+char Tile::getTileName()
+{
+    char c ;
+    if(tileType == R)
+    {
+        c = 'R';
+    }
+    else if (tileType == Y)
+    {
+        c = 'Y';
+    }
+    else if (tileType == B)
+    {
+		c = 'B';
+    }
+    else if (tileType == L)
+    {
+        c = 'L';
+    }
+    else if (tileType == U)
+    {
+		c = 'U';
+    }
+    else if (tileType == F)
+    {
+		c = 'F';
+    }
+    else
+    {
+		c = '.';
+    }
+	return c;
+}
+
+void Tile::setTileType(TileType typeNeedToSet)
+{
+  this->tileType = typeNeedToSet;
+}
